4dddfddf2b9e896395a55891274b3b58544dcd17
BlamireJarred-ST <jarredblamire01@gmail.com>
2026-09-14 21:30:21 +0200

    updated TelemetryPacket.cs

--- SmartX.Shared/TelemetryPacket.cs
+++ SmartX.Shared/TelemetryPacket.cs
@@ -1,13 +1,19 @@
 ﻿
-namespace SmartX.Shared.Models; 
+namespace SmartX.Shared.Models;
 
+// Represents a telemetry packet containing data from a device
 public class TelemetryPacket<T>
 {
+    // Unique identifier for the device sending the telemetry data
     public string DeviceId { get; set; }
+    // Timestamp indicating when the telemetry data was generated
     public DateTime Timestamp { get; set; }
+    // The actual telemetry data of type T
     public T Data { get; set; }
+    // Type of the telemetry packet
     public string PacketType { get; set; }
 
+    // Initializes a new instance of the TelemetryPacket class 
     public TelemetryPacket(string deviceId, T data, string packetType)
     {
         DeviceId = deviceId;
@@ -16,6 +22,7 @@ public class TelemetryPacket<T>
         PacketType = packetType;
     }
 
+    // Returns a string representation of the telemetry packet
     public override string ToString()
         => $"[{PacketType}] Station {DeviceId} @ {Timestamp:HH:mm:ss} => {Data}";
 }
