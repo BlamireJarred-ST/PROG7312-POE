a9e1ccfc6d50b0d4e6f865d232ea1e9eef7f15fc
BlamireJarred-ST <jarredblamire01@gmail.com>
2026-09-14 21:08:01 +0200

    Update Sensor.cs

--- SmartX.Shared/Models/Sensor.cs
+++ SmartX.Shared/Models/Sensor.cs
@@ -1,18 +1,25 @@
 ﻿
 namespace SmartX.Shared.Models;
 
+// Sensor identification
 public class Sensor
 {
+    // Sensor MAC address
     public string MacAddress { get; set; } = string.Empty;
-
+    // Sensor Location
     public string Room { get; set; } = string.Empty;
+    // Sensor Zone
     public string Zone { get; set; } = string.Empty;
+    // Sensor Node ID
     public string NodeId { get; set; } = string.Empty;
 
+    // Sensor Category
     public SensorCategory Category { get; set; }
 
+    // Sensor Registration Timestamp
     public DateTime RegisteredAt { get; set; }
 
+    // New sensor creation timestamp
     public Sensor()
     {
         RegisteredAt = DateTime.UtcNow;
