22910be5c4f1464014cf62b5c6a0d91284ae2d67
BlamireJarred-ST <jarredblamire01@gmail.com>
2026-09-14 21:13:22 +0200

    Update SensorCategory.cs

--- SmartX.Shared/Models/SensorCategory.cs
+++ SmartX.Shared/Models/SensorCategory.cs
@@ -3,10 +3,14 @@ using System.Text.Json.Serialization;
 
 namespace SmartX.Shared.Models;
 
+// Defines the category of a sensor, which can be Environmental, PowerConsumption, or Actuator.
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum SensorCategory
 {
+    // Measures temprature
     Environmental,
+    // Measures power in watts
     PowerConsumption,
+    // Monitiors valves
     Actuator
 }
