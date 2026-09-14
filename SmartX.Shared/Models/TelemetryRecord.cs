534c203209c34937c4602b285ec0edef457ff6bf
BlamireJarred-ST <jarredblamire01@gmail.com>
2026-09-14 21:33:16 +0200

    Udated TelemetryRecord.cs

--- SmartX.Shared/Models/TelemetryRecord.cs
+++ SmartX.Shared/Models/TelemetryRecord.cs
@@ -3,9 +3,14 @@ namespace SmartX.Shared.Models;
 
 public class TelemetryRecord
 {
+    // ID for the device sending the telemetry data
     public string DeviceId { get; set; } = string.Empty;
+    // Timestamp indicating when the telemetry data was generated
     public DateTime Timestamp { get; set; }
+    // The actual telemetry data as a string
     public string PacketType { get; set; } = string.Empty;
+    // The actual telemetry data as a string
     public string DataValue { get; set; } = string.Empty;
+    // Severity level of the telemetry data, defaulting to = "Normal"
     public string Severity { get; set; } = "Normal";
 }
