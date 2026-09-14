210ca0d2e4782d95dfad731417967780580803ef
BlamireJarred-ST <jarredblamire01@gmail.com>
2026-09-14 21:35:28 +0200

    Udated TelemetryBatchRequest

--- SmartX.Shared/Models/TelemetryBatchRequest.cs
+++ SmartX.Shared/Models/TelemetryBatchRequest.cs
@@ -1,7 +1,10 @@
 ﻿namespace SmartX.Shared.Models;
 
+// Represents a batch request for telemetry data from multiple devices
 public class TelemetryBatchRequest
 {
+    // Array of device IDs for which telemetry data is requested
     public string[] DeviceIds { get; set; } = Array.Empty<string>();
+    // 2D array of telemetry data batches corresponding to the device IDs
     public double[][] Batches { get; set; } = Array.Empty<double[]>();
 }
